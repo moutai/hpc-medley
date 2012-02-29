44c60ca3c2db90ee4dca56846a80041d5a7138fd
Moussa Taifi <moutai10@gmail.com>
2012-02-29 01:52:21 +0000

    adding ft and bt

--- NPB3.3.1/NPB3.3-MPI/bin/launchbench.py
+++ NPB3.3.1/NPB3.3-MPI/bin/launchbench.py
@@ -19,7 +19,7 @@ for currentbench in listbench.readlines():
 	#print benchname
 	#print benchsize
 	#print benchprocs
-	for npernode in range(1,3):
+	for npernode in range(1,5):
 		bin_name= str(benchname)+'.'+str(benchsize)+'.'+str(benchprocs)
 		resultfilename = bin_name+"."+str(npernode)+"-results.txt"
 		command2run= "mpirun -np "+ str(benchprocs)+" -npernode " +str(npernode)+" -hostfile computehosts.txt ./"+str(bin_name)+" | tee "+ resultfilename
