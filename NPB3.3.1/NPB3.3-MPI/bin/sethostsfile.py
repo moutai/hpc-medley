e701cfe58c5d8f05a5b90a01245edd4ba34e884b
Moussa Taifi <moutai10@gmail.com>
2012-03-01 15:52:35 +0000

    added per vm benchmark run

---
+++ NPB3.3.1/NPB3.3-MPI/bin/sethostsfile.py
@@ -0,0 +1,10 @@
+import sys
+
+def sethosts(numhosts):
+	f = open ("computehosts.txt","w")
+	#numhosts=sys.argv[1]
+	print numhosts
+	for i in range(1,numhosts+1):
+		f.write("clusternode"+str(i-1)+"\n")
+
+	f.close()
